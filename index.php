02fbd0f48fd43d46d3d8fd17f4166094b5564fbc
MudhithaK <mudhithakalpani08@gmail.com>
2024-07-22 11:12:55 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>hello world</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        <!--script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script-->
+        
+    </head>
+    <body>
+        
+        <div class="container">
+            <h1>Test for a bootstrap table</h1>
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th>Name :</th>
+                         <th>Address :</th>
+                          <th>NIC No :</th>
+                    </tr>
+                    <tr>
+                        <td>W.P.M.Kalpani</td>
+                        <td>Myland state, Kadawalagedara</td>
+                        <td>123345678</td>
+                    </tr>
+                </thead>
+            </table>
+        </div>
+        
+        <div class="container">
+            <h1>Test for a bootstrap table</h1>
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th>Name :</th>
+                         <th>Address :</th>
+                          <th>NIC No :</th>
+                    </tr>
+                    <tr>
+                        <td>W.P.M.Kalpani</td>
+                        <td>Myland state, Kadawalagedara</td>
+                        <td>123345678</td>
+                    </tr>
+                </thead>
+            </table>
+        </div>
+        
+        <div class="container">
+            <h1>Test for a bootstrap table</h1>
+            <table class="table table-sprided">
+                <thead>
+                    <tr>
+                        <th>Name :</th>
+                         <th>Address :</th>
+                          <th>NIC No :</th>
+                    </tr>
+                    <tr>
+                        <td>W.P.M.Kalpani</td>
+                        <td>Myland state, Kadawalagedara</td>
+                        <td>123345678</td>
+                    </tr>
+                </thead>
+            </table>
+        </div>
+        
+        <div class="container">
+            <h1>Test for a bootstrap table</h1>
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th>Name :</th>
+                         <th>Address :</th>
+                          <th>NIC No :</th>
+                    </tr>
+                    <tr class="success">
+                        <td>W.P.M.Kalpani</td>
+                        <td>Myland state, Kadawalagedara</td>
+                        <td>123345678</td>
+                    </tr>
+                     <tr class="info">
+                        <td>W.P.M.Kalpani</td>
+                        <td>Myland state, Kadawalagedara</td>
+                        <td>123345678</td>
+                    </tr>
+                    <tr class="danger">
+                        <td>THE END</td>
+                    </tr>
+                </thead>
+            </table>
+        </div>
+        
+        
+        <div class="container">
+            <h1>Test for a bootstrap table</h1>
+        <div class="row">
+            <div class="col-sm-4" style="background-color: yellow">33.33 present</div>
+            <div class="col-sm-8" style="background-color: blue">66.67 present</div>
+        </div>
+        </div>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
